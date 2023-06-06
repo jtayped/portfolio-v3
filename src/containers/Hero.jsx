9bee96d3072a6f40a417449666dbf8e00412a30a
Joel Taylor <jtayped@gmail.com>
2023-06-06 16:34:20 +0200

    height/font weight

--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -12,16 +12,14 @@ const Hero = () => {
   return (
     <main className="flex flex-col lg:flex-row lg:justify-between items-center h-[80vh]">
       <div className="p-10 sm:p-[50px] lg:p-[100px] 2xl:p-[200px] h-full border-black border-b-[1px] flex flex-col justify-center">
-        <h1 className="text-6xl xl:text-8xl font-normal sm:font-bold">
-          {hero.title}
-        </h1>
+        <h1 className="text-6xl xl:text-8xl">{hero.title}</h1>
         <p className="text-md sm:text-lg">{hero.description}</p>
         <button className="rounded border-black border-[1px] w-min px-2 py-1 text-lg mt-2 flex items-center gap-2 hover:bg-black hover:text-white transition-colors duration-150">
           <FiMousePointer size={18} />
           Button
         </button>
       </div>
-      <div className="grid grid-cols-2 lg:grid-rows-2 lg:grid-cols-1 h-full">
+      <div className="grid grid-cols-2 lg:grid-rows-2 lg:grid-cols-1 md:h-full">
         {hero.secondarySections.map((section) => (
           <div className="p-[25px] lg:p-[50px] 2xl:p-[100px] border-b-[1px] border-black border-l-[1px] flex flex-col sm:justify-center">
             <h2 className="font-bold sm:font-normal text-md sm:text-2xl xl:text-4xl">
