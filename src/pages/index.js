d45fd03dc75df09cd4605d9d2b9ac4233b3ebb97
Joel Taylor <jtayped@gmail.com>
2023-06-14 18:06:25 +0200

    pages removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,1 @@
 export { default as Home } from "./Home";
-export { default as NotFound } from "./NotFound";
-export { default as Contact } from "./Contact";
-export { default as Portfolio } from "./Portfolio";
-export { default as About } from "./About";
